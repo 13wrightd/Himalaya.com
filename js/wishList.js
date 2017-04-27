5bec33f78a3469c329926e5ef108b2b8e2a0a7d8
patsfanx247 <jth5305@psu.edu>
2017-04-27 13:01:37 -0400

    fixed values of posting item category

--- js/wishList.js
+++ js/wishList.js
@@ -1,10 +1,7 @@
 var socket = io();
 
 $(document).ready(function(){
-	console.log('document ready');
 
-		
-	$('#showWishList').click(function(){
 	var msg=
 	{
   		itemName: $('#itemName').val(),
@@ -16,10 +13,21 @@ $(document).ready(function(){
   		username: localStorage.getItem('username'),
   		sessionString: localStorage.getItem('sessionString'),
   		itemLocation: $('#itemLocation').val()
-	}
+	};
 	
-  		console.log(msg);
-		socket.emit('post auction', msg);
+		socket.emit('wishList', msg);
+
+		socket.on('search results', function(msg){
+		console.log(msg);
+		$('#resultList').empty();
+		jQuery.each( msg, function( i, val ) {
+			//var imgString='<img src='
+   			//$("#searchList").append("<li  class='list-group-item'>"+ val.comment+'<span style="float:right">'+val.star+'</span>'+'</li>');
+	//style='width:50%'
+		$('#resultList').append("<li  class='valign-wrapper list-group-item'>"+"<a href='item_page.html?id="+msg[i]._id+"'> <img class='valign'"+' style="width:20%; " src="' + msg[i].url + '" /> '  +msg[i].title+"<span style='float:right'>$"+msg[i].price+'</span><a></li>');
+
+			onClick='item_page.html?id="+msg[i]._id+"'
+
 	});
 	
 });
